28c9138420cfeaba0605c49f52abcfbc9d8b60ab
vyalkov <kingbingping@gmail.com>
2023-04-11 12:48:33 +0500

    finish

--- services/file_handling.py
+++ services/file_handling.py
@@ -1,4 +1,4 @@
-BOOK_PATH = 'book/Чистый-Python.-Тонкости-программирования-для-профи-_-PDFDrive-_.txt'
+BOOK_PATH = '/Users/vlad/BOT/bookbot/book/book_python.txt'
 PAGE_SIZE = 1050
 book: dict[int, str] = {}
 
@@ -6,13 +6,15 @@ book: dict[int, str] = {}
 # Функция, возвращающая строку с текстом страницы и ее размер
 def _get_part_text(text: str, start: int, size: int):
     marks = ',.!:;?'
-    new_txt = text[start:start+size]
-    if new_txt[-1] == '.':
-        for i in text[start+size + 1:]:
-            if i == '.':
-                new_txt += i
-            else:
+    new_txt = text[start:start + size]
+    ln_size = 0
+    if new_txt[-1] == '.' and new_txt[-2] in marks:
+        for i in range(start + size - 2, 0, -1):
+            if text[i] == ' ' and text[i - 1] in marks:
                 break
+            ln_size += 1
+
+        return text[start:start + size - ln_size - 2], len(text[start:start + size - ln_size - 2])
 
     if new_txt[-1] not in marks:
         for i in range(1, len(new_txt)):
@@ -24,14 +26,14 @@ def _get_part_text(text: str, start: int, size: int):
 
 # Функция, формирующая словарь книги
 def prepare_book(path: str) -> None:
-    with open(path, 'r') as file:
+    with open(path) as file:
         text = file.read()
-    start, page_number = 0, 1
-    while start < len(text):
-        page_text, page_size = _get_part_text(text, start, PAGE_SIZE)
-        start += page_size
-        book[page_number] = page_text.strip()
-        page_number += 1
+        start, page_number = 0, 1
+        while start < len(text):
+            page_text, page_size = _get_part_text(text, start, PAGE_SIZE)
+            start += page_size
+            book[page_number] = page_text.strip()
+            page_number += 1
 
 
 # Вызов функции prepare_book для подготовки книги из текстового файла
